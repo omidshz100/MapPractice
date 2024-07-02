c22677a57afbd76e53c5fd2118213bf2716ab59a
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-07-02 09:40:32 +0200

    Initial Commit

---
+++ MapPractice/MapPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPracticeApp.swift
+//  MapPractice
+//
+//  Created by Omid Shojaeian Zanjani on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MapPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
